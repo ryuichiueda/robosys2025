b7ed2c00c356567842f2fcd857bda7cad153bbae
Ryuichi Ueda <ryuichiueda@gmail.com>
2025-10-30 12:50:24 +0900

    Add strange input test cases

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX ...
 
 ng () {
@@ -7,8 +7,19 @@ ng () {
 }
 
 res=0
+
+### NORMAL INPUT ###
 a=$(seq 5 | ./plus)
 [ "$a" = 15 ] || ng "$LINENO" 
+
+### STRANGE INPUT ###
+a=$(echo あ | ./plus)
+[ "$?" = 1 ] || ng "$LINENO" 
+[ "$a" = "" ] || ng "$LINENO" 
+
+a=$(echo | ./plus)
+[ "$?" = 1 ] || ng "$LINENO" 
+[ "$a" = "" ] || ng "$LINENO" 
 
 [ "$res" = 0 ] && echo OK
 
